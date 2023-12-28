1a3273f176af73196dd52eaf51d0c09f038753bc
Harshit15046 <shrihari15046@gmail.com>
2023-12-28 12:40:19 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const operation= require('./operations');
+console.log(operation.add(2,3));//5
+console.log(operation.multiply(2,3));//6
+console.log(operation.sub(34,34))//
---
+++ operations.js
@@ -0,0 +1,13 @@
+module.exports.add=function(a,b)
+{
+    return a+b;
+}
+
+module.exports.multiply=function(a,b)
+{
+    return a*b;
+}
+module.exports.sub=function(a,b)
+{
+    return a-b;
+}
